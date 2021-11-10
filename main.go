1aff2962a134c1a7c1ddcc0ee36203bdc2289ab6
milinches <ibrogenius5165@gmail.com>
2021-11-10 23:12:13 +0100

    exchangerate using golang

--- main.go
+++ main.go
@@ -10,15 +10,18 @@ import (
 )
 
 func main() {
+	// Loading the env file which contains your unique API_KEY
 	if err := godotenv.Load(".env"); err != nil {
 		log.Println("Error loading .env file")
 	}
 	api_key := os.Getenv("API_KEY")
 
+	// Making a request to the API
 	response, err := http.Get(api_key)
 	if err != nil {
 		log.Println(err)
 	}
+	
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Println(err)
